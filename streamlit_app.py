babe22690aab63f587acd5e42c02b42d25b1a777
amarimonyea <amarijones12@yahoo.com>
2026-02-27 19:36:41 +0000

    Restructure: use streamlit_app.py as router, move Overview to pages/01_Overview, renumber other pages

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Gender Equality Tracker", layout="wide")
+
+# Immediately switch to the overview page
+st.switch_page("pages/01_Overview.py")
