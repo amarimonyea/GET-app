bf3854062b11a9189314a5258010fdfbbbda4394
amarimonyea <amarijones12@yahoo.com>
2026-03-03 14:20:02 +0000

    Increase left padding on Net Direction chart for more y-axis white space

--- pages/01_Overview.py
+++ pages/01_Overview.py
@@ -801,7 +801,7 @@ else:
     # zero line
     zero_line = alt.Chart(pd.DataFrame({"x": [0]})).mark_rule(strokeDash=[6, 4]).encode(x="x:Q")
 
-    st.altair_chart((domain_chart + zero_line).properties(padding={"left": 40, "right": 40, "top": 20, "bottom": 20}), use_container_width=True)
+    st.altair_chart((domain_chart + zero_line).properties(padding={"left": 120, "right": 40, "top": 20, "bottom": 20}), use_container_width=True)
 
     # optional: quick table for export/readability
     with st.expander("See domain totals"):
