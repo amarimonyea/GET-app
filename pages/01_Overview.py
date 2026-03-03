a2e93818201afda9da0f29b3bc5d352dcfc220a5
amarimonyea <amarijones12@yahoo.com>
2026-03-03 17:46:57 +0000

    Increase font size for subtitle caption

--- pages/01_Overview.py
+++ pages/01_Overview.py
@@ -246,7 +246,7 @@ SECTOR_COL = "Sector Impacted"
 # 5) PAGE TITLE
 # ---------------------------
 st.title("🌍 Gender Equality Tracker")
-st.caption("Tracking directional shifts in the U.S. gender policy landscape")
+st.markdown("<p style='font-size: 1.3rem; color: rgba(27, 23, 37, 0.85); margin-top: -20px;'>Tracking directional shifts in the U.S. gender policy landscape</p>", unsafe_allow_html=True)
 
 # ---------------------------
 # 6) TOP METRICS
